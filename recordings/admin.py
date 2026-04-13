c4ea25a4d6a30aa5de4298c860ad46e23d84b342
Moon <moonrahman05@gmail.com>
2026-04-14 04:06:52 +0930

    Project Title Added

--- recordings/admin.py
+++ recordings/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-    
-from .models import Species, Location, Recording, Anomaly
 
-admin.site.register(Species)
-admin.site.register(Location)
-admin.site.register(Recording)
-admin.site.register(Anomaly)
+# Register your models here.
