aefd3bceeaa3056543cfdb5ea2cc0c49296e44f5
hau <github2105@hotmail.com>
2026-04-16 04:17:26 +0930

    Admin Updated

--- recordings/admin.py
+++ recordings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Species, Location, Recording, Anomaly
 
-# Register your models here.
+admin.site.register(Species)
+admin.site.register(Location)
+admin.site.register(Recording)
+admin.site.register(Anomaly)
