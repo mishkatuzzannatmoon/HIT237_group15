49065efc031da6bf16eaba6b90f6a345accb7ad5
hau <github2105@hotmail.com>
2026-04-18 20:28:30 +0930

    views.py Updated
    Add class-based views for Species, AudioRecording and AnomalyFlag
    
    Implements ListView, DetailView, CreateView, UpdateView and DeleteView
    for all three models. RecordingListView delegates filter logic to
    RecordingQuerySet.filter_by_params(). Context data passes model choices
    to templates for filter dropdowns.

--- recordings/views.py
+++ recordings/views.py
@@ -1,140 +1,131 @@
-from django.shortcuts import render
-from django.views import View
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Species, Location, Recording, Anomaly
 from django.urls import reverse_lazy
-
-
-recordings_data = [
-    {
-        "id": 1,
-        "species": "Northern Quoll",
-        "date": "2026-04-02",
-        "location": "Darwin",
-        "confidence_score": 92
-    },
-    {
-        "id": 2,
-        "species": "Black-footed Tree-rat",
-        "date": "2026-04-01",
-        "location": "Kakadu",
-        "confidence_score": 87
-    },
-    {
-        "id": 3,
-        "species": "Gouldian Finch",
-        "date": "2026-03-30",
-        "location": "Arnhem Land",
-        "confidence_score": 95
-    }
-]
-
-
-class RecordingListView(View):
-    def get(self, request):
-        return render(request, "recordings/recording_list.html", {
-            "recordings": recordings_data
-        })
-
-
-class RecordingDetailView(View):
-    def get(self, request, id):
-        selected_recording = None
-
-        for recording in recordings_data:
-            if recording["id"] == id:
-                selected_recording = recording
-                break
-
-        return render(request, "recordings/recording_detail.html", {
-            "recording": selected_recording
-        })
-
-
-class UploadRecordingView(View):
-    def get(self, request):
-        return render(request, "recordings/upload_recording.html")
-
-    def post(self, request):
-        species = request.POST.get("species")
-        message = f"{species} uploaded successfully!"
-
-        return render(request, "recordings/upload_recording.html", {
-            "message": message
-        })
-
-
+from .models import Species, AudioRecording, AnomalyFlag, ConservationStatus, RecordType
+from .forms import SpeciesForm, AudioRecordingForm, AnomalyFlagForm
+ 
 class SpeciesListView(ListView):
-    model = Species
-    template_name = 'species/species_list.html'
-    context_object_name = 'species'
-
-class ThreatenedSpeciesListView(SpeciesListView):
-    template_name = 'species/threatened_species.html'
-    def get_queryset(self):
-        return Species.objects.threatened()
-
+    model               = Species
+    template_name       = 'recordings/species_list.html'
+    context_object_name = 'species_list'
+ 
+ 
+class SpeciesDetailView(DetailView):
+    model         = Species
+    template_name = 'recordings/species_detail.html'
+ 
+ 
 class SpeciesCreateView(CreateView):
-    model = Species
-    fields = '__all__'
-    success_url = reverse_lazy('species_list')
-
+    model         = Species
+    form_class    = SpeciesForm
+    template_name = 'recordings/species_form.html'
+    success_url   = reverse_lazy('species_list')
+ 
+ 
 class SpeciesUpdateView(UpdateView):
-    model = Species
-    fields = '__all__'
-    success_url = reverse_lazy('species_list')
-
+    model         = Species
+    form_class    = SpeciesForm
+    template_name = 'recordings/species_form.html'
+    success_url   = reverse_lazy('species_list')
+ 
+ 
 class SpeciesDeleteView(DeleteView):
-    model = Species
-    success_url = reverse_lazy('species_list')
-
-
-
+    model         = Species
+    template_name = 'recordings/species_confirm_delete.html'
+    success_url   = reverse_lazy('species_list')
+ 
 class RecordingListView(ListView):
-    model = Recording
-    template_name = 'recordings/recording_list.html'
+    model               = AudioRecording
+    template_name       = 'recordings/recording_list.html'
     context_object_name = 'recordings'
-    queryset = Recording.objects.select_related('species', 'location', 'user')
-
-class HighConfidenceRecordingListView(RecordingListView):
-
-    template_name = 'recordings/high_confidence.html'
+ 
     def get_queryset(self):
-        return Recording.objects.high_confidence().select_related('species', 'location', 'user')
+        return (
+        AudioRecording.objects
+        .with_details()
+        .filter_by_params(self.request)
+    )
+ 
+    def get_context_data(self, **kwargs):
 
+        context = super().get_context_data(**kwargs)
+        context['all_species']           = Species.objects.order_by('common_name')
+        context['conservation_choices']  = ConservationStatus.choices
+        context['record_type_choices']   = RecordType.choices
+        return context
+ 
+class RecordingDetailView(DetailView):
+    model         = AudioRecording
+    template_name = 'recordings/recording_detail.html'
+ 
+    def get_queryset(self):
+        return AudioRecording.objects.select_related('species', 'recorded_by').prefetch_related('flags')
+ 
+ 
 class RecordingCreateView(CreateView):
-    model = Recording
-    fields = ['species', 'location', 'record_type', 'date_recorded', 'confidence_score']
-    success_url = reverse_lazy('recording_list')
-
-
-
+    model         = AudioRecording
+    form_class    = AudioRecordingForm
+    template_name = 'recordings/recording_form.html'
+    success_url   = reverse_lazy('recording_list')
+ 
+    def form_valid(self, form):
+        if self.request.user.is_authenticated:
+            form.instance.recorded_by = self.request.user
+        return super().form_valid(form)
+ 
+ 
+class RecordingUpdateView(UpdateView):
+    model         = AudioRecording
+    form_class    = AudioRecordingForm
+    template_name = 'recordings/recording_form.html'
+    success_url   = reverse_lazy('recording_list')
+ 
+ 
+class RecordingDeleteView(DeleteView):
+    model         = AudioRecording
+    template_name = 'recordings/recording_confirm_delete.html'
+    success_url   = reverse_lazy('recording_list')
+ 
 class AnomalyListView(ListView):
-    model = Anomaly
-    template_name = 'anomalies/anomaly_list.html'
+    model               = AnomalyFlag
+    template_name       = 'recordings/anomaly_list.html'
     context_object_name = 'anomalies'
-    queryset = Anomaly.objects.select_related('recording', 'recording__species')
-
-class FlaggedAnomalyListView(AnomalyListView):
-    template_name = 'anomalies/flagged.html'
+ 
     def get_queryset(self):
-        return Anomaly.objects.flagged()
-
-class NeedsReviewAnomalyListView(AnomalyListView):
-    template_name = 'anomalies/review.html'
-    def get_queryset(self):
-        return Anomaly.objects.needs_review()
-
+        return AnomalyFlag.objects.select_related('recording__species', 'flagged_by')
+ 
 class AnomalyCreateView(CreateView):
-    model = Anomaly
-    fields = ['recording', 'reason']
-    success_url = reverse_lazy('anomaly_list')
-
+    model         = AnomalyFlag
+    form_class    = AnomalyFlagForm
+    template_name = 'recordings/anomaly_form.html'
+    success_url   = reverse_lazy('anomaly_list')
+ 
+    def form_valid(self, form):
+        form.instance.recording_id = self.kwargs['recording_pk']
+        if self.request.user.is_authenticated:
+            form.instance.flagged_by = self.request.user
+        recording = AudioRecording.objects.get(pk=self.kwargs['recording_pk'])
+        recording.flag_as_anomaly(
+            reason=form.cleaned_data['reason'],
+            flagged_by=form.instance.flagged_by,
+        )
+        return super().form_valid(form)
+ 
+ 
 class AnomalyUpdateView(UpdateView):
-    model = Anomaly
-    fields = ['reason', 'status']
-    success_url = reverse_lazy('anomaly_list')
+    model         = AnomalyFlag
+    form_class    = AnomalyFlagForm
+    template_name = 'recordings/anomaly_form.html'
+    success_url   = reverse_lazy('anomaly_list')
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        flag = self.object
+        if flag.resolved:
+            flag.recording.resolve_flags()
+        return response
+ 
+ 
 class AnomalyDeleteView(DeleteView):
-    model = Anomaly
-    success_url = reverse_lazy('anomaly_list')
+    model         = AnomalyFlag
+    template_name = 'recordings/anomaly_confirm_delete.html'
+    success_url   = reverse_lazy('anomaly_list')
