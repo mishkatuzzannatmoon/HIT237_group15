aa41dc6da47ea02dcb02c1e9120a6765cda80284
hau <github2105@hotmail.com>
2026-04-18 20:21:32 +0930

    Admin.py Updated

--- recordings/admin.py
+++ recordings/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from .models import Species, Location, Recording, Anomaly
+from .models import Species, AudioRecording, AnomalyFlag
 
-admin.site.register(Species)
-admin.site.register(Location)
-admin.site.register(Recording)
-admin.site.register(Anomaly)
+
+@admin.register(Species)
+class SpeciesAdmin(admin.ModelAdmin):
+    list_display  = ['common_name', 'scientific_name', 'conservation_status', 'is_native', 'not_native']
+    list_filter   = ['conservation_status', 'is_native', 'not_native']
+    search_fields = ['common_name', 'scientific_name']
+    ordering      = ['scientific_name']
+
+
+class AnomalyFlagInline(admin.TabularInline):
+    model  = AnomalyFlag
+    extra  = 0
+    fields = ['reason', 'resolved', 'flagged_by']
+    readonly_fields = ['flagged_by']
+
+
+@admin.register(AudioRecording)
+class AudioRecordingAdmin(admin.ModelAdmin):
+    list_display  = ['species', 'location_name', 'recorded_at', 'confidence_score', 'is_anomaly']
+    list_filter   = ['is_anomaly', 'species__conservation_status']
+    search_fields = ['species__common_name', 'location_name']
+    ordering      = ['-recorded_at']
+    inlines       = [AnomalyFlagInline]
+
+
+@admin.register(AnomalyFlag)
+class AnomalyFlagAdmin(admin.ModelAdmin):
+    list_display  = ['recording', 'flagged_by', 'resolved']
+    list_filter   = ['resolved']
+    search_fields = ['recording__species__common_name', 'reason']
