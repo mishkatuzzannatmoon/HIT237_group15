48680dcb5e0b02507c039d97c32cbc46f59c0edc
hau <github2105@hotmail.com>
2026-05-27 07:37:48 +0930

    Update forms.py

--- recordings/forms.py
+++ recordings/forms.py
@@ -5,20 +5,11 @@ from .models import Species, AudioRecording, AnomalyFlag
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
     class Meta:
         model  = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
 
- 
 class SpeciesForm(forms.ModelForm):
     class Meta:
         model = Species
@@ -47,4 +38,4 @@ class AnomalyFlagForm(forms.ModelForm):
         widgets = {
             'reason': forms.Textarea(attrs={'rows': 3}),
         }
- 
+
